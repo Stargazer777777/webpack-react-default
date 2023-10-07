1bbfdfda61de86769f20b9afb527686d5aa613b1
stargazer <1601778118@qq.com>
2023-10-07 11:03:14 +0800

    update http interceptor; change title

--- src/$http/index.ts
+++ src/$http/index.ts
@@ -1,53 +1,94 @@
 import axios, { AxiosError } from 'axios';
 import type { AxiosRequestConfig } from 'axios';
-export const httpInstance = axios.create();
 
-export type BkResponse = {
-  data: unknown; // 出错时这一项将没有
-  code: number;
+export interface BkResponse {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  data: any;
   msg: string;
   success: boolean;
-};
+}
 
-httpInstance.defaults.baseURL = import.meta.env.HTTP_BASEURL;
+export interface BkErrorResponse extends BkResponse {
+  detail: string;
+}
 
 export interface HttpOption {
   noAlert?: true;
 }
 
-export const $http = async (
-  config: AxiosRequestConfig,
-  httpOption: HttpOption
-) => {
-  try {
-    const axiosResponse = await httpInstance<BkResponse>(config);
-    const bkResponse = axiosResponse.data;
-
-    if (!bkResponse?.success) {
-      let errTitle = 'Error';
-      if (bkResponse.code === 401) {
-        errTitle = 'Unauthorized';
-      } else if (bkResponse.code === 403) {
-        errTitle = 'Forbidden';
-      } else if (bkResponse.code === 406) {
-        errTitle = '406Error';
-      } else if (bkResponse.code === 500) {
-        errTitle = 'ServerError';
-      } else {
-        errTitle = 'Unknown';
-      }
-      if (!httpOption.noAlert) {
-        alert(`${errTitle}: ${bkResponse.msg || 'unknown'}`);
-      }
-      const err = new Error(bkResponse?.msg || 'Unknown');
-      err.name = errTitle;
-      throw err;
-    }
-    return bkResponse;
-  } catch (err) {
-    if (err instanceof AxiosError) {
-      alert('网络错误');
-    }
-    throw err;
+export class BkError extends Error {
+  constructor(bkErrorResponse: BkErrorResponse) {
+    super();
+    super.name = 'BkError';
+    super.message = bkErrorResponse.msg || 'Unknown error';
+    super.cause = bkErrorResponse.detail;
   }
-};
+}
+
+class HttpTool {
+  protected httpInstance = axios.create();
+
+  constructor(baseUrl?: string) {
+    if (baseUrl) {
+      this.setBaseUrl(baseUrl);
+    }
+  }
+
+  setBaseUrl(baseUrl: string) {
+    this.httpInstance.defaults.baseURL = baseUrl;
+  }
+
+  setAuthorization(Authorization: string) {
+    this.httpInstance.defaults.headers.common['Authorization'] = Authorization;
+  }
+
+  removeAuthorization() {
+    delete this.httpInstance.defaults.headers.common['Authorization'];
+  }
+
+  protected errHandler(err: AxiosError, httpOption?: HttpOption) {
+    const statusCode = err.response?.status;
+
+    let errMsg = '未知错误';
+
+    let bkError: BkError | undefined;
+
+    if (err.response?.data) {
+      const errResponse = err.response.data as BkErrorResponse;
+      bkError = new BkError(errResponse);
+      errMsg = errResponse.msg || '未知错误';
+    }
+
+    switch (statusCode) {
+      case 401:
+        break;
+      case 403:
+        break;
+      default:
+        break;
+    }
+
+    if (!httpOption?.noAlert) {
+      alert(errMsg);
+    }
+    throw bkError;
+  }
+
+  async send(
+    config: AxiosRequestConfig,
+    httpOption?: HttpOption
+  ): Promise<BkResponse> {
+    try {
+      const axiosResponse = await this.httpInstance<BkResponse>(config);
+      return axiosResponse.data as BkResponse;
+    } catch (err) {
+      if (err instanceof AxiosError) {
+        throw this.errHandler(err, httpOption);
+      } else {
+        throw err;
+      }
+    }
+  }
+}
+
+export const httpTool = new HttpTool(import.meta.env['HTTP_BASE']);
